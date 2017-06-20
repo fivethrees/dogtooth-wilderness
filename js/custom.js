fa30247935760543c38364d551b67fc529888f3b
Alice Clanton <alicepholden@hotmail.com>
2017-06-19 20:12:10 -0400

    Changed images and worked on Introduction responsiveness

--- js/custom.js
+++ js/custom.js
@@ -1,58 +1,12 @@
 window.onload = function() {
   fade();
-  cloudOne();
-  cloudTwo();
   arrow();
-  forest1();
 }
   function fade() {
-    if (window.matchMedia("(max-width: 576px)").matches) {
       $("#intro").hide().fadeIn(2000);
-      $("#forest1").hide();
-    } else {
-      $("#intro").hide();
-      setTimeout(
-        function() {
-          $("#intro").hide().fadeIn(2000);
-        }, 3000);
-    }
   }
-  function cloudOne() {
-    setInterval(
-      function() {
-        $("#cloud1").animate({
-          left: $(window).width()}, 45000,
-          function() { $(this).removeAttr('style'); });
-      }, 1000);
-  }
-  function cloudTwo() {
-    setInterval(
-      function() {
-        $("#cloud2").animate({
-          left: $(window).width()}, 50000,
-          function() { $(this).removeAttr('style'); });
-      }, 1000);
-  }
+  
   function arrow() {
-    if (window.matchMedia("(max-width: 576px)").matches) {
-      $(".arrow").animate({
-        bottom: '5px'}, 2000);
-    } else {
-      setTimeout(
-        function() {
-          $(".arrow").animate({
-            bottom: '5px'}, 2000);
-        }, 3000);
-    }
-  }
-  function forest1() {
-    if (window.matchMedia("(min-width: 576px)").matches) {
-      $("#forest1phone").hide();
-      $("#forest2phone").hide();
-      $("#forest3phone").hide();
-      $("#forest4phone").hide();
-    }
-    // if (window.matchMedia("(min-width: 576px)").matches) {
-    //   $("#introduction").hide().fadeIn(2000);
-    // }
+    $(".arrow").animate({
+      bottom: '5px'}, 2000);
   }
