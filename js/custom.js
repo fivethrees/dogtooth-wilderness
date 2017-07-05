a2bd3cbb7b46077761fcab75f5fb9731282475ab
Alice Clanton <alicepholden@hotmail.com>
2017-07-05 19:26:31 -0400

    Added javascript and fixed nav menu

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,14 @@
-window.onLoad() {
-  fade();
+window.onload = function() {
+  fadeWanderSafely();
+  fadeSentence();
 }
-
-function fade() {
-  
+function fadeWanderSafely() {
+  $("#wander-safely").hide().fadeIn(3000);
 }
+function fadeSentence() {
+  $("#sentence").hide();
+    setTimeout(
+      function() {
+        $("#sentence").hide().fadeIn(3000);
+      }, 2000);
+  }
